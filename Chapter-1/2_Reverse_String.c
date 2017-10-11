42e74d544ed8a61df934951f9fe910807be0c285
Hsekiu <kpetr7@gmail.com>
2017-10-10 20:28:56 -0700

    Updated solution 1.1.

--- Chapter-1/2_Reverse_String.c
+++ Chapter-1/2_Reverse_String.c
@@ -4,9 +4,11 @@
 
 
 char* reverse(char *input) {
+    //Temp var to hold for switching characters
     char temp;
     int length = strlen(input)-1;
     int start, end;
+    //Loop through string going towards center and switching characters
     for(start = 0, end = length; start < end; start++, end--) {
         temp = input[start];
         input[start] = input[end];
@@ -17,7 +19,6 @@ char* reverse(char *input) {
 }
 
 int main(int argc, char **argv) {
-    char temp;
     char *input = argv[1];
     
     printf("%s", reverse(input));
