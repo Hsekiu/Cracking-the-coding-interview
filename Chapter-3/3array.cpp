810d1891482cc315e8aa87b3909ff52a4e33449a
Hsekiu <kpetr7@gmail.com>
2017-10-26 23:18:19 -0700

    Fixed solution 3.1.

--- Chapter-3/3array.cpp
+++ Chapter-3/3array.cpp
@@ -1,53 +1,101 @@
 #include <stdio.h>
 #include <iostream>
+#include <math.h>
 
 class arrStack {
     
     private:
-        int stack[10] = {0};
-        int size = 1;
-        int middle = 4, head = 0, tail = 9;
-        int midPos = 0;
+        int stack[10];
+        int s1head;
+        int s2head;
+        int s3head;
+        int middle;
+        float midCons;
     
     public:
-        
-        bool isEmpty() {
-            return (size == 1);
+
+        void init() {
+            for(int i = 0; i < (sizeof(stack)/sizeof(int)); i++) {
+                stack[i] = 0;
+            }
+            s1head = 0;
+            s2head = 4;
+            s3head = 9;
+            middle = 4;
+            midCons = 0;
         }
         
-        int pop() {
-            if(size != 0) {
-                size--;
-                return stack[size-1];
+        bool isEmpty(int list) {
+            if(list == 1) {
+                return (s1head == 0);
+            } else if(list == 2) {
+                return (s2head == 4);
+            } else if (list == 3) {
+                return (s3head == 9);
+            } else {
+                return 0;
             }
-            return 0;
+        }
+        
+        int pop(int list) {
+            int tmp;
+            if(list == 1) {
+                tmp = stack[s1head - 1];
+                stack[s1head - 1] = 0;
+                s1head--;
+                return tmp;
+            } else if(list == 2) {
+                int midPos;
+                midCons -= 0.5;
+                if(ceil(midCons) == midCons) {
+                    midPos = midCons;  
+                } else {
+                    midPos = -1*ceil(midCons);
+                }
+                tmp = stack[middle + midPos];
+                stack[middle + midPos] = 0;
+                return tmp;
+            } else if(list == 3) {
+                tmp = stack[s3head + 1];
+                stack[s3head + 1] = 0;
+                s3head++;
+                return tmp;
+            } else {
+                return 0;
+            }
         }
         
         void push(int list, int data) {
             if(list == 1) {
-                stack[head] = data;
-                head++;
+                stack[s1head] = data;
+                s1head++;
             } else if (list == 2) {
-                if(midPos < 0) {
-                    stack[middle + midPos] = data;
-                    std::cout << midPos << std::endl;
-                    midPos *= -1;
-                } else if (midPos == 0) {
-                    stack[middle] = data;
-                    midPos = -1;
+                int midPos;
+                if(ceil(midCons) == midCons) {
+                    midPos = midCons;  
                 } else {
-                    stack[middle + midPos] = data;
-                    std::cout << midPos << std::endl;
-                    midPos *= -1;
-                    midPos--;
+                    midPos = -1*ceil(midCons);
                 }
+                midCons += 0.5;
+                stack[middle + midPos] = data;
+                s2head = middle + midPos;
             } else if (list == 3) {
-                stack[tail] = data;
-                tail--;
+                stack[s3head] = data;
+                s3head--;
             } else {
-                return;
             }
-            size++;
+        }
+        
+        int peek(int list) {
+            if(list == 1) {
+                return stack[s1head - 1];
+            } else if (list == 2) {
+                return stack[s2head];
+            } else if (list == 3) {
+                return stack[s3head + 1];
+            } else {
+                return 0;
+            }
         }
         
         void print() {
@@ -62,18 +110,31 @@ class arrStack {
 int main() {
     
     arrStack stack1;
+    stack1.init();
     stack1.push(2, 1);
+    stack1.print();
     stack1.push(2, 2);
+    stack1.print();
     stack1.push(2, 3);
+    stack1.print();
     stack1.push(2, 4);
+    stack1.print();
     stack1.push(1, 5);
+    stack1.print();
     stack1.push(3, 6);
     stack1.print();
-    std::cout << (stack1.isEmpty() ? "true" : "false") << std::endl;
-    //std::cout << stack1.pop() << std::endl;
-    //std::cout << stack1.pop() << std::endl;
-    //std::cout << stack1.pop() << std::endl;
-    //std::cout << (stack1.isEmpty() ? "true" : "false") << std::endl;
+    stack1.push(3, 99);
+    stack1.print();
+
+    std::cout << "Top of stack 1 is: " << stack1.peek(1) << std::endl;
+    std::cout << "Top of stack 2 is: " << stack1.peek(2) << std::endl;
+    std::cout << "Top of stack 3 is: " << stack1.peek(3) << std::endl;
+    std::cout << (stack1.isEmpty(1) ? "true" : "false") << std::endl;
+    std::cout << stack1.pop(1) << std::endl;
+    std::cout << stack1.pop(2) << std::endl;
+    std::cout << stack1.pop(3) << std::endl;
+    std::cout << stack1.pop(2) << std::endl;
+    stack1.print();
     
     return 0;
 }
