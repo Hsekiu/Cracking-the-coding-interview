5a011d753595df86767ab425772c6ddc49dfa2f9
Hsekiu <kpetr7@gmail.com>
2017-10-29 23:21:35 -0700

    Added solution 3.4.

---
+++ Chapter-3/4_queueStack.cpp
@@ -0,0 +1,84 @@
+#include "StackBase.h"
+
+/*
+    To make a Queue using two stacks use one to hold the data and the other as
+    temporary storage. When you pop from the inStack and push to the outStack you
+    reverse the order. Adding is simple since with the stack you add to the front.
+    With remove you have to loop through the stack before the last element and then
+    push all the data back.
+*/
+
+class myQueue : public StackBase {
+
+    public:
+
+    void init (int n[], int num) {
+        size = num;
+        for(int i = 0; i < num; i++) {
+            inStack.push(n[i]);
+        }
+    }
+
+    void add(int data) {
+        inStack.push(data);
+        size++;
+    }
+
+    int remove() {
+        size--;
+
+        //Loop through the stack and push all but the last element to the working stack.
+        for(int i = 0; i < size; i++) {
+            outStack.push(inStack.pop());
+        }
+        //Remove the last element.
+        int result = inStack.pop();
+
+        //Transfer data back from working stack to data stack.
+        for(int i = 0; i < size; i++) {
+            inStack.push(outStack.pop());
+        }
+
+        return result;
+    }
+
+    void print() {
+        //When transfering data between stack it reverses them.
+        for(int i = 0; i < size; i++) {
+            outStack.push(inStack.pop());
+        }
+
+         outStack.print();
+
+        //Data stack is now empty have to push all the data back after printing.
+        for(int i = 0; i < size; i++) {
+            inStack.push(outStack.pop());
+        }
+    }
+
+	private:
+
+    //Stack to hold the data.
+	StackBase inStack;
+    //Temp working stack.
+    StackBase outStack;
+    int size;
+
+};
+
+int main() {
+
+	myQueue queue;
+	int data[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+	queue.init(data, sizeof(data)/sizeof(int));
+	std::cout << "Queue is: ";
+    queue.print();
+    std::cout << "Adding 12 to Queue: ";
+    queue.add(12);
+    queue.print();
+	std::cout << "Removing oldest element: " << queue.remove() << std::endl;
+    std::cout << "Removing oldest element: " << queue.remove() << std::endl;
+    std::cout << "Queue is: ";
+	queue.print();
+	return 0;
+}
